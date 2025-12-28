bc95f6923f3119185e7c8d38667b92b05cb55665
wh0oo <45668419+wh0oo@users.noreply.github.com>
2025-12-28 17:27:10 -0600

    Create CheeriosMod class for mod initialization

---
+++ src/main/java/com/example/cheerios/CheeriosMod.java
@@ -0,0 +1,10 @@
+package com.example.cheerios;
+
+import net.fabricmc.api.ModInitializer;
+
+public class CheeriosMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        // Intentionally empty for now
+    }
+}
