4028df3af83f8a4a0e5310a8d2a5bae1ebc9fff3
wh0oo <45668419+wh0oo@users.noreply.github.com>
2025-12-29 11:56:01 -0600

    Register Log4j filter during mod initialization
    Added a Log4j filter to the CheeriosMod initialization process.

--- src/main/java/com/example/cheerios/CheeriosMod.java
+++ src/main/java/com/example/cheerios/CheeriosMod.java
@@ -1,13 +1,30 @@
 package com.example.cheerios;
 
 import com.example.cheerios.config.CheeriosConfig;
+import com.example.cheerios.logging.BlockAttachedEntityLogFilter;
 import net.fabricmc.api.ModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.LoggerContext;
 
 public class CheeriosMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		// Load TOML config
 		CheeriosConfig.load();
+
+		// Register Log4j filter
+		registerLogFilter();
+	}
+
+	private void registerLogFilter() {
+		LoggerContext context = (LoggerContext) LogManager.getContext(false);
+
+		BlockAttachedEntityLogFilter filter =
+				new BlockAttachedEntityLogFilter();
+
+		context.getConfiguration().addFilter(filter);
+		context.updateLoggers();
 	}
 }
 
